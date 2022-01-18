0c10a5ee64335af73b9812d413c3402b6766b8f5
Caner <kurucaner@gmail.com>
2022-01-18 16:22:04 -0500

    Add basic functioanlity

--- src/screens/ListScreen.tsx
+++ src/screens/ListScreen.tsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState, ChangeEvent, KeyboardEvent } from "react";
+import { nanoid } from "nanoid";
 
 type Props = {};
 
+type Task = {
+  id: string;
+  label: string;
+};
+
 const ListScreen: React.FC<Props> = () => {
-  return <div>List view 2</div>;
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTaskLabel, setNewTaskLabel] = useState("");
+
+  const handleNewTaskLabelChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setNewTaskLabel(e.target.value);
+  };
+
+  const handleNewTaskKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter" && newTaskLabel.length > 0) {
+      setTasks((tasks) => [{ id: nanoid(), label: newTaskLabel }, ...tasks]);
+      setNewTaskLabel("");
+      console.log(tasks);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        value={newTaskLabel}
+        onChange={handleNewTaskLabelChange}
+        onKeyPress={handleNewTaskKeyPress}
+      />
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>{task.label}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ListScreen;
